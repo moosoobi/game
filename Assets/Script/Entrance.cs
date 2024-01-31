0fdef5eb9c445a4b81a75bb125a7dba0d7adf486
moosoobi <tmxkcksgh@gmail.com>
2024-01-31 11:43:58 +0900

    1
    1

--- Assets/Script/Entrance.cs
+++ Assets/Script/Entrance.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class Entrance : MonoBehaviour
 {
-    public SceneController Scene; 
     private void OnTriggerEnter(Collider other){
         if (other.CompareTag("Player")){
-            Scene.LoadNextScene();
         }
     }
 
