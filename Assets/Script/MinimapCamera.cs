87ff274a54c150638772ebf14c572abb17110b3f
moosoobi <tmxkcksgh@gmail.com>
2024-02-06 01:20:30 +0900

    1
    1

---
+++ Assets/Script/MinimapCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            // 플레이어의 위치를 정확히 따라가기
+            transform.position = new Vector3(player.position.x, player.position.y+100f, player.transform.position.z);
+        }
+    }
+}
