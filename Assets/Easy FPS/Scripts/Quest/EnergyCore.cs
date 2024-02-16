24b12dca388aac8f3f679ac51ec9c03781a45f56
moosoobi <tmxkcksgh@gmail.com>
2024-02-16 23:51:22 +0900

    1
    1

--- Assets/Easy FPS/Scripts/Quest/EnergyCore.cs
+++ Assets/Easy FPS/Scripts/Quest/EnergyCore.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class EnergyCore : Quest
@@ -8,6 +9,7 @@ public class EnergyCore : Quest
 
     public QuestState CurrentState;
     public string[] dialogue;
+    public string[] dialogue1;
     public int curResponseTracker=0;
     public TextMeshProUGUI npcName;
     public TextMeshProUGUI npcDialogueBox;
@@ -15,6 +17,13 @@ public class EnergyCore : Quest
     public bool isTalking=false;
     private bool zzz=false;
     public GunInventory guninventory;
+    public AudioSource SecuritySound;
+    static public float CoreHp=10f;
+    public float CoreMaxHp=10f;
+    public TextMeshProUGUI Core;
+    public Slider healthSlider;
+    public bool first=false;
+
      public EnergyCore(QuestState currentState)
     {
         CurrentState=currentState;
@@ -44,13 +53,22 @@ public class EnergyCore : Quest
         dialogueUI.SetActive(true);
         npcName.text=name;
         npcDialogueBox.text=dialogue[0];
-
-
     }
 
     private void OnTriggerEnter(Collider other)
     {    
             zzz=true;
+            if (other.CompareTag("Attack")){
+                if(!first){
+                    
+                    healthSlider.gameObject.SetActive(true);
+                    Core.gameObject.SetActive(true);
+                    first=true;
+                    healthSlider.maxValue = CoreMaxHp;
+                    healthSlider.value = CoreHp;
+                }
+                UpdateHealth(-1f);
+        }
     }
     private void OnTriggerExit(Collider other)
     {    
@@ -69,7 +87,21 @@ public class EnergyCore : Quest
     public void EndDialogue(){
         isTalking=false;
         dialogueUI.SetActive(false);
-
     }
+    public void UpdateHealth(float newHP)
+    {
+
+        CoreHp += newHP;
+
+        // 슬라이더에 반영
+        healthSlider.value = CoreHp;
+
+        // HP가 0 이하로 떨어졌을 때 처리 (예를 들어, 보스가 죽었을 때)
+        if (CoreHp <= 0f)
+        {
+            Debug.Log("파괴");
+        }
+    }
+
 
 }
