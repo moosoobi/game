c5a6b2884fd43cc9dd6e9bc1defc8c53ea98ea6f
moosoobi <tmxkcksgh@gmail.com>
2024-02-23 01:09:49 +0900

    키오스크 구현
    키오스크구현

--- Assets/bar/kioskUI/Script/MouseCursor.cs
+++ Assets/bar/kioskUI/Script/MouseCursor.cs
@@ -8,6 +8,29 @@ public class MouseCursor : MonoBehaviour
     public RectTransform uiRectTransform;
     public GameObject JackCoke;
     public GameObject JackCokeMouseOver;
+    public GameObject Xrated;
+    public GameObject XratedMouseOver;
+    public GameObject Mojito;
+    public GameObject MojitoMouseOver;
+    public GameObject Blue;
+    public GameObject BlueMouseOver;
+    public GameObject Kahlua;
+    public GameObject KahluaMouseOver;
+    public GameObject Espresso;
+    public GameObject EspressoMouseOver;
+    public GameObject Black;
+    public GameObject BlackMouseOver;
+    public GameObject Tequila;
+    public GameObject TequilaMouseOver;
+    public GameObject Illegal;
+    public GameObject IllegalMouseOver;
+    public GameObject White;
+    public GameObject WhiteMouseOver;
+    public GameObject Peach;
+    public GameObject PeachMouseOver;
+    public GameObject Rusty;
+    public GameObject RustyMouseOver;
+    public float moveSpeed = 5f;
 
 
     void Start()
@@ -34,6 +57,164 @@ public class MouseCursor : MonoBehaviour
             JackCoke.SetActive(true);
             JackCokeMouseOver.SetActive(false);
         }
+        if(currentX>-109f&&currentX<0f&&currentY>97f&&currentY<222f){
+            Xrated.SetActive(false);
+            XratedMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            Xrated.SetActive(true);
+            XratedMouseOver.SetActive(false);
+        }
+        if(currentX>22f&&currentX<128f&&currentY>97f&&currentY<222f){
+            Mojito.SetActive(false);
+            MojitoMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            Mojito.SetActive(true);
+            MojitoMouseOver.SetActive(false);
+        }
+        if(currentX>150f&&currentX<250f&&currentY>97f&&currentY<222f){
+            Blue.SetActive(false);
+            BlueMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            Blue.SetActive(true);
+            BlueMouseOver.SetActive(false);
+        }
+        if(currentX>-234f&&currentX<-130f&&currentY>-61f&&currentY<66f){
+            Kahlua.SetActive(false);
+            KahluaMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            Kahlua.SetActive(true);
+            KahluaMouseOver.SetActive(false);
+        }
+        if(currentX>-109f&&currentX<0f&&currentY>-61f&&currentY<66f){
+            Espresso.SetActive(false);
+            EspressoMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            Espresso.SetActive(true);
+            EspressoMouseOver.SetActive(false);
+        }
+        if(currentX>22f&&currentX<128f&&currentY>-61f&&currentY<66f){
+            Tequila.SetActive(false);
+            TequilaMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            Tequila.SetActive(true);
+            TequilaMouseOver.SetActive(false);
+        }
+        if(currentX>150f&&currentX<250f&&currentY>-61f&&currentY<66f){
+            JackCoke.SetActive(false);
+            JackCokeMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            JackCoke.SetActive(true);
+            JackCokeMouseOver.SetActive(false);
+        }
+        if(currentX>-234f&&currentX<-130f&&currentY>-213f&&currentY<-83f){
+            JackCoke.SetActive(false);
+            JackCokeMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            JackCoke.SetActive(true);
+            JackCokeMouseOver.SetActive(false);
+        }
+        if(currentX>-109f&&currentX<0f&&currentY>-213f&&currentY<-83f){
+            JackCoke.SetActive(false);
+            JackCokeMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            JackCoke.SetActive(true);
+            JackCokeMouseOver.SetActive(false);
+        }
+        if(currentX>22f&&currentX<128f&&currentY>-213f&&currentY<-83f){
+            JackCoke.SetActive(false);
+            JackCokeMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            JackCoke.SetActive(true);
+            JackCokeMouseOver.SetActive(false);
+        }
+        if(currentX>150f&&currentX<250f&&currentY>-213f&&currentY<-83f){
+            JackCoke.SetActive(false);
+            JackCokeMouseOver.SetActive(true);
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            */
+        }else{
+            JackCoke.SetActive(true);
+            JackCokeMouseOver.SetActive(false);
+        }
+        
+        float horizontalInput = Input.GetAxis("Mouse X");
+        float verticalInput = Input.GetAxis("Mouse Y");
+
+        // 입력에 따라 이동 방향 설정
+        Vector2 moveDirection = new Vector2(horizontalInput, verticalInput);
+
+        // 현재 anchoredPosition 가져오기
+        Vector2 currentPosition = uiRectTransform.anchoredPosition;
+
+        // 입력에 따라 이동한 위치 계산
+        Vector2 newPosition = currentPosition + moveDirection*moveSpeed * Time.deltaTime;
+
+        // 새로 계산된 위치로 anchoredPosition 설정
+        uiRectTransform.anchoredPosition = newPosition;
         
     }
 }
