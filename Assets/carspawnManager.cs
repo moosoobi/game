721a2f5bd1f48ff066d40f2eaa2ea84ae8d2ecbc
hmg1560 <parkmonica2@naver.com>
2024-03-21 03:46:40 +0900

    자동차 추가

---
+++ Assets/carspawnManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carspawnManager : MonoBehaviour
+{
+    public int maxCount;
+    public int carCount;
+    public Transform[] spawnPoints;
+    public GameObject[] car;
+
+    public float spawnTime = 3f;
+    public float curTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curTime >= spawnTime && carCount < maxCount)
+        {
+            int x = Random.Range(0, spawnPoints.Length);
+            int y = Random.Range(0, car.Length);
+            SpawnCar(x, y);
+
+        }
+        curTime += Time.deltaTime;
+
+    }
+
+    public void SpawnCar(int spawnN, int carN)
+    {
+        curTime = 0;
+        carCount++;
+        Instantiate(car[carN], spawnPoints[spawnN]);
+
+    }
+
+}
