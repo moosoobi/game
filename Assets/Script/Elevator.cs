850940338bdcaf16c094f8cdbe9754c8a67e42e9
moosoobi <tmxkcksgh@gmail.com>
2024-04-08 00:50:55 +0900

    Update Elevator.cs

--- Assets/Script/Elevator.cs
+++ Assets/Script/Elevator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Elevator : MonoBehaviour
 {
@@ -10,8 +11,13 @@ public class Elevator : MonoBehaviour
     public float moveSpeed = 2f;
 
     public bool movingUp = false;
+    public bool First=true;
     
     public GameObject player;
+
+    public TextMeshProUGUI npcName;
+    public TextMeshProUGUI npcDialogueBox;
+    public GameObject dialogueUI;
   
 
     // Update is called once per frame
@@ -33,8 +39,29 @@ public class Elevator : MonoBehaviour
         
             if (other.CompareTag("Player")){
                 movingUp=true;
+                if(First){
+                    First=false;
+                    StartCoroutine(StartConversation());
+                }
             }
         
+        
+    }
+
+    IEnumerator StartConversation()
+    {
+        yield return new WaitForSeconds(3.0f);
+        dialogueUI.SetActive(true);
+        npcName.text=" ";
+        npcDialogueBox.text="…항상 아래에서 위를 쳐다보기만 했었는데";
+        yield return new WaitForSeconds(3.0f);
+        npcDialogueBox.text="… …";
+        yield return new WaitForSeconds(3.0f);
+        npcDialogueBox.text="위에서 내려다 보는 도시는 꽤 아름답네";
+        yield return new WaitForSeconds(3.0f);
+        dialogueUI.SetActive(false);
+
+
         
     }
 }
